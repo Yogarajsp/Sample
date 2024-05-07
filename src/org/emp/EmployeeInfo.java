925335f4c602ac39e2e61f1b97ca7c4501ee209b
Yogarajsp <spyogarajmech@gmail.com>
2024-05-07 17:17:58 +0530

    Employee Project Added

---
+++ src/org/emp/EmployeeInfo.java
@@ -0,0 +1,51 @@
+package org.emp;
+
+public class EmployeeInfo {
+	
+	private void empId() {
+		
+		System.out.println("2001");
+	}
+	
+	private void empName() {
+		
+		System.out.println("YOGARAJ");
+	}
+	
+	private void empDob() {
+		
+		System.out.println("17-03-2001");
+	}
+	
+	private void empPhone() {
+		
+		System.out.println("9790322296");
+
+	}
+	
+	private void empEmail() {
+		
+		System.out.println("spyogarajmech@gmail.com");
+
+	}
+	
+	private void empAddress() {
+		
+		System.out.println("Paramathi Velur");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		EmployeeInfo e=new EmployeeInfo();
+		
+		e.empAddress();
+		e.empDob();
+		e.empEmail();
+		e.empId();
+		e.empName();
+		e.empPhone();
+		
+	}
+
+}
